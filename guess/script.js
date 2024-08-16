40fca83809bbbf6fcdbed8df6e5d41464c97cbae
AlainDLC <jd_alain@hotmail.com>
2024-08-16 19:53:59 +0200

    reperting the learning skills :)

--- guess/script.js
+++ guess/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let secret = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('🧌 No Number!!');
+  } else if (guess === secret) {
+    displayMessage("😎 YEESS YOU D'IT IT");
+    document.querySelector('.number').textContent = secret;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secret) {
+    if (score > 1) {
+      displayMessage(guess > secret ? 'Too High' : 'Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('❌ You LOSE');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secret = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
